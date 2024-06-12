3c0e7fa6d75c93c6b5859a912de103e411c8b5fa
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-06-12 18:09:31 +0900

    feat : DragoonEnemyStateEnum, DragoonEenmyIdleState

--- Assets/01.Scripts/Agent/Enemy/DragoonEnemy/DragoonEnemy.cs
+++ Assets/01.Scripts/Agent/Enemy/DragoonEnemy/DragoonEnemy.cs
@@ -1,11 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DragoonEnemyStateEnum
+{
+    Idle,
+    Battle,
+    Attack,
+    Dead
+}
+
 public class DragoonEnemy : Enemy
 {
+    public EnemyStateMachine<DragoonEnemyStateEnum> StateMachine { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        StateMachine = new EnemyStateMachine<DragoonEnemyStateEnum>();
+        foreach (DragoonEnemyStateEnum stateEnum in Enum.GetValues(typeof(DragoonEnemyStateEnum)))
+        {
+            string typeName = stateEnum.ToString();
+            try
+            {
+                Type t = Type.GetType($"DragoonEnemy{typeName}State");
+                EnemyState<DragoonEnemyStateEnum> enemyState = Activator.CreateInstance(t, this, StateMachine, typeName) as EnemyState<DragoonEnemyStateEnum>;
+
+                StateMachine.AddState(stateEnum, enemyState);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"{typeName} doesn't exist! : {e.Message}");
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StateMachine.Initialize(DragoonEnemyStateEnum.Idle, this);
+    }
+
+    private void Update()
+    {
+        StateMachine.CurrentState.UpdateState();
+    }
+
+
     public override void AnimationEndTrigger()
     {
-
+        StateMachine.CurrentState.AnimationFinishTrigger();
     }
 }
