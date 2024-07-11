e7553f306aa717bb40c78bfafbadeddf07c53af5
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-11 20:29:08 +0900

    feat: Red Zone

--- Assets/01.Scripts/Combat/RedZone/RedZoneManager.cs
+++ Assets/01.Scripts/Combat/RedZone/RedZoneManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class RedZoneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private DecalProjector _redZoneDecal;
+    [SerializeField] private float _redZoneDuration = 10f;
+    [SerializeField] private WarningUI _warningUI;
+    
+    public void Start()
     {
-        
+        _redZoneDecal.gameObject.SetActive(false);
+    }
+    
+    public void StartRedZone(Vector3 position, string warningMessage)
+    {
+        _redZoneDecal.transform.position = position;
+        _redZoneDecal.gameObject.SetActive(true);
+        _redZoneDecal.transform.DOScale(new Vector3(10,10, 10), 0.5f);
+        _warningUI.OpenWarningPanel(warningMessage, 0.2f);
+        StartCoroutine(EndRedZone());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator EndRedZone()
     {
-        
+        yield return new WaitForSeconds(2f);
+        _warningUI.CloseWarningPanel(0.2f);
+        yield return new WaitForSeconds(_redZoneDuration);
+        _redZoneDecal.transform.DOScale(new Vector3(0,0, 10), 0.5f)
+            .OnComplete(() => _redZoneDecal.gameObject.SetActive(false));
     }
 }
