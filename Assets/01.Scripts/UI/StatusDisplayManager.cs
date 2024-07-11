62204df3ed2712ae7111cc68b98b0ce0d8c4880a
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-07-11 19:21:02 +0900

    feat: Health

--- Assets/01.Scripts/UI/StatusDisplayManager.cs
+++ Assets/01.Scripts/UI/StatusDisplayManager.cs
@@ -5,7 +5,8 @@ public class StatusDisplayManager : MonoBehaviour
 {
     [SerializeField] private WeaponDisplay _weaponDisplay_Left;
     [SerializeField] private WeaponDisplay _weaponDisplay_Right;
-
+    [SerializeField] private HealthUI _healthUI; 
+        
     private PlayerPart _currentPart;
     private CanvasGroup _canvasGroup;    
     
@@ -14,6 +15,8 @@ public class StatusDisplayManager : MonoBehaviour
         _currentPart = PlayerPartController.Instance.currentPlayerPart;
         _currentPart.magazineInfoL.playerOverloadEvent += _weaponDisplay_Left.HandleDisplayRefresh;
         _currentPart.magazineInfoR.playerOverloadEvent += _weaponDisplay_Right.HandleDisplayRefresh;
+        GameManager.Instance.Player.HealthCompo.OnHealthChangedEvent.AddListener(_healthUI.Refresh);
+        // 체력 이벤트 등록하기
     }
 
     public void SetVisible(bool value)
