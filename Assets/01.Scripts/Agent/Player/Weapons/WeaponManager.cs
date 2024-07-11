ae843584e9014d9646f8b1c7136787c46f04451a
Crogen0407 <mymcoding0407@gmail.com>
2024-07-12 06:13:57 +0900

    feat : Weapon System Clear!

--- Assets/01.Scripts/Agent/Player/Weapons/WeaponManager.cs
+++ Assets/01.Scripts/Agent/Player/Weapons/WeaponManager.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
 	[field:SerializeField] public WeaponDataSO WeaponDataSO { get; private set; }
-	public List<WeaponEnum> currentActiveWeapon;
+	private Dictionary<WeaponEnum, Weapon> _currentWeaponDictionary;
+	public List<WeaponEnum> startWeapon;
+	private Transform _weaponParent;
 
 	private void Start()
 	{
-		Transform weaponParent = PlayerPartController.Instance.currentPlayerPart.transform.Find("Weapons");
+		_weaponParent = PlayerPartController.Instance.currentPlayerPart.transform.Find("Weapons");
+		_currentWeaponDictionary = new Dictionary<WeaponEnum, Weapon>();
 
-		for (int i = 0; i < currentActiveWeapon.Count; ++i)
+		for (int i = 0; i < startWeapon.Count; ++i)
 		{
-			if (WeaponDataSO.weaponDataDictionary[currentActiveWeapon[i]].weaponPrefab == null) continue;
-
-			WeaponData currentWeaponData = WeaponDataSO.weaponDataDictionary[currentActiveWeapon[i]];
-
-			Weapon weapon =	Instantiate(currentWeaponData.weaponPrefab, weaponParent);
-			weapon.maxAttackDelay = currentWeaponData.coolTime;
-			weapon.currentAttackDelay = currentWeaponData.coolTime;
+			if (WeaponDataSO.weaponDataDictionary[startWeapon[i]].weaponPrefab == null) continue;
+			AppendWeapon(startWeapon[i]);
 		}
+	}
+
+	public void AppendWeapon(WeaponEnum weaponEnum)
+	{
+		WeaponData currentWeaponData = WeaponDataSO.weaponDataDictionary[weaponEnum];
+
+		Weapon weapon = Instantiate(currentWeaponData.weaponPrefab, _weaponParent);
+		weapon.maxAttackDelay = currentWeaponData.coolTime;
+		weapon.currentAttackDelay = currentWeaponData.coolTime;
+
+		_currentWeaponDictionary.Add(weaponEnum, weapon);
+	}
+
+	public void UseWeapon(WeaponEnum weaponEnum)
+	{
+		if(_currentWeaponDictionary.ContainsKey(weaponEnum) == false)
+		{
+			Debug.LogWarning("This Weapon is not created");
+			return;
+		}
+
+		Debug.Log("Use Weapon!");
+		_currentWeaponDictionary[weaponEnum].OnAttack();
 	}
 }
