eca650678abc5e3a7ef387a0e09b59957a0e5530
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-15 19:49:05 +0900

    feat: Red

---
+++ Assets/01.Scripts/Combat/RedZone/RedZone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class RedZone : MonoBehaviour
+{
+    public SphereCollider AreaCollider { get; private set; }
+
+    private void Awake()
+    {
+        AreaCollider = GetComponent<SphereCollider>();
+    }
+    
+    public void RedZoneSet(Vector3 position, Vector3 scale, Ease ease,TweenCallback onComplete = null, float openDuration = 0.5f)
+    {
+        transform.position = position;
+        transform.DOScale(scale, openDuration).SetEase(ease)
+            .OnComplete(onComplete);
+    }
+}
--- Assets/01.Scripts/Combat/RedZone/RedZoneManager.cs
+++ Assets/01.Scripts/Combat/RedZone/RedZoneManager.cs
@@ -6,20 +6,23 @@ using UnityEngine.Rendering.Universal;
 
 public class RedZoneManager : MonoBehaviour
 {
-    [SerializeField] private DecalProjector _redZoneDecal;
+    [SerializeField] private RedZone _redZoneDecal;
     [SerializeField] private float _redZoneDuration = 10f;
     [SerializeField] private WarningUI _warningUI;
     
     public void Start()
     {
         _redZoneDecal.gameObject.SetActive(false);
+        StartRedZone(Vector3.zero, "Warning! Red Zone is coming!");
     }
     
     public void StartRedZone(Vector3 position, string warningMessage)
     {
-        _redZoneDecal.transform.position = position;
+        _redZoneDecal.AreaCollider.enabled = false;
         _redZoneDecal.gameObject.SetActive(true);
-        _redZoneDecal.transform.DOScale(new Vector3(10,10, 10), 0.5f);
+        _redZoneDecal.RedZoneSet(position, 
+                new Vector3(10, 10, 10), Ease.InSine, 
+                () => _redZoneDecal.AreaCollider.enabled = true);
         _warningUI.OpenWarningPanel(warningMessage, 0.2f);
         StartCoroutine(EndRedZone());
     }
@@ -29,7 +32,8 @@ public class RedZoneManager : MonoBehaviour
         yield return new WaitForSeconds(2f);
         _warningUI.CloseWarningPanel(0.2f);
         yield return new WaitForSeconds(_redZoneDuration);
-        _redZoneDecal.transform.DOScale(new Vector3(0,0, 10), 0.5f)
-            .OnComplete(() => _redZoneDecal.gameObject.SetActive(false));
+        _redZoneDecal.RedZoneSet(transform.position,
+            new Vector3(0, 0, 10), Ease.OutSine,
+            () => _redZoneDecal.gameObject.SetActive(false));
     }
 }
