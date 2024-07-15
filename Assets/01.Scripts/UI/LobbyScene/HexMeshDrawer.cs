e5746a648b7f464550eb8acbdd34d3f7d0d8d210
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-07-15 22:55:28 +0900

    feat: IWindowPanel

---
+++ Assets/01.Scripts/UI/LobbyScene/HexMeshDrawer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMeshDrawer : MonoBehaviour
+{
+    private float[] _prevFillAmount;
+    
+    public void DrawHexGraph(float[] values)
+    {
+        // 유까켱을 구려야합니다
+        
+        
+    }
+    
+    
+    
+    
+
+}
