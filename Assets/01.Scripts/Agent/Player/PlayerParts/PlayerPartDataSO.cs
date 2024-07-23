daf41e8dd8d15c5e465af8e40df90ee98271ae55
Crogen0407 <mymcoding0407@gmail.com>
2024-07-23 18:07:22 +0900

    feat : StatData

--- Assets/01.Scripts/Agent/Player/PlayerParts/PlayerPartDataSO.cs
+++ Assets/01.Scripts/Agent/Player/PlayerParts/PlayerPartDataSO.cs
@@ -7,6 +7,7 @@ public class PlayerPartDataSO : ScriptableObject
     public string partName;
     public PlayerPart partPrefab;
     public Sprite partImage;
+    public StatDataSO statData;
 
     [Header("Status Setting")] 
     public float damage;
