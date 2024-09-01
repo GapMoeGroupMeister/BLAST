f23b9af96fbd306b4157207248e28b1e63c3487e
Crogen0407 <mymcoding0407@gmail.com>
2024-09-02 00:15:54 +0900

    feat : Footprint

--- Assets/01.Scripts/Combat/DamageCaster/CircleRayDamageCaster.cs
+++ Assets/01.Scripts/Combat/DamageCaster/CircleRayDamageCaster.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class CircleRayDamageCaster : DamageCaster
 {
-	[Header("CircleRayCaster")]
+	[Header("CircleRayCaster (¹Ì¿Ï¼º)")]
 	public Vector3 center;
-	public int count;
-	[Range(0f, 360f)]
+	public uint count;
+	[Range(0f, 300f)]
 	public float angle;
 	[Range(0f, 360f)]
 	public float rotation;
@@ -22,13 +22,20 @@ public class CircleRayDamageCaster : DamageCaster
 		if (excluded) Gizmos.color = Color.red;
 		else Gizmos.color = Color.green;
 
-		for (int i = 0; i <= count-1; ++i)
+		if(count==1)
 		{
-			float curAngle = angle * ((float)i/count);
-
-			Gizmos.DrawRay(center + transform.position, Quaternion.Euler(0, curAngle, 0) * Vector3.forward * radius);
+			Gizmos.DrawRay(center + transform.position, Quaternion.Euler(0, 45 - angle*0.5f, 0) * Vector3.forward * radius);
 		}
+		else
+		{
+			for (int i = 0; i < count; ++i)
+			{
+				float curAngle = angle * ((float)i / (count - 1));
 
+				Gizmos.DrawRay(center + transform.position, Quaternion.Euler(0, curAngle - angle*0.5f, 0) * Vector3.forward * radius);
+			}
+		}
+		
 		Gizmos.color = Color.white;
 	}
 }
