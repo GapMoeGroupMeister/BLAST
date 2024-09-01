d1e4fb5f2eba848d9637c4d9ce9addaf7ce52c70
Crogen0407 <mymcoding0407@gmail.com>
2024-09-01 12:03:45 +0900

    feat : AttackPointRotation

---
+++ Assets/01.Scripts/Agent/Player/PlayerParts/PlayerHoleMakerPart.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerHoleMakerPart : PlayerPart
+{
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+		magazineInfoR.OnAttackEvent += AttackPointRotationR;
+		magazineInfoL.OnAttackEvent += AttackPointRotationL;
+	}
+
+	private void OnDisable()
+	{
+		magazineInfoR.OnAttackEvent -= AttackPointRotationR;
+		magazineInfoL.OnAttackEvent -= AttackPointRotationL;
+	}
+
+	private void AttackPointRotationR(Vector3 obj)
+	{
+		for (int i = 0; i < magazineInfoR.bulletFirePositions.Length; ++i)
+		{
+			Vector3 originRot = magazineInfoR.bulletFirePositions[i].localEulerAngles + (Vector3.forward * 180);
+
+			magazineInfoR.bulletFirePositions[i].DOLocalRotate(originRot, 0.1f);
+		}
+	}
+
+	private void AttackPointRotationL(Vector3 obj)
+	{
+		for (int i = 0; i < magazineInfoL.bulletFirePositions.Length; ++i)
+		{
+			Vector3 originRot = magazineInfoL.bulletFirePositions[i].localEulerAngles + (Vector3.forward * 180);
+
+			magazineInfoL.bulletFirePositions[i].DOLocalRotate(originRot, 0.1f);
+		}
+	}
+}
