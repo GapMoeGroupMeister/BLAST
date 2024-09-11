27907f631af4eb971f06d2604032ad5fb4e1d4dd
Crogen0407 <mymcoding0407@gmail.com>
2024-09-11 18:06:12 +0900

    feat : Dash Complete

---
+++ Assets/01.Scripts/Agent/Player/State/PlayerDashState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerDashState : PlayerState<PlayerStateEnum>
+{
+	PlayerMovement playerMovement;
+
+    public PlayerDashState(Player playerBase, PlayerStateMachine<PlayerStateEnum> stateMachine, string animBoolName) : base(playerBase, stateMachine, animBoolName)
+    {
+		playerMovement = playerBase.MovementCompo as PlayerMovement;
+	}
+
+	public override void Enter()
+	{
+		base.Enter();
+		float duration = 0.65f;
+		playerMovement.OnDash(playerMovement.transform.forward, duration, playerMovement.dashPower);
+		_playerBase.StartCoroutine(CoroutineOnDashEnd(duration));
+	}
+
+	private IEnumerator CoroutineOnDashEnd(float dashDuration)
+	{
+		yield return new WaitForSeconds(dashDuration);
+		_stateMachine.ChangeState(PlayerStateEnum.Idle);
+	}
+
+	public override void Exit()
+	{
+		base.Exit();
+	}
+}
