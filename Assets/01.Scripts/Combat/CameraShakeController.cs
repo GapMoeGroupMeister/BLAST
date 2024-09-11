6c6157ebc7d642c4598feaf9c9585c9becc4e0e0
Leooo <123617759+Leooo918@users.noreply.github.com>
2024-09-11 23:08:39 +0900

    feat: camera shake controller

--- Assets/01.Scripts/Combat/CameraShakeController.cs
+++ Assets/01.Scripts/Combat/CameraShakeController.cs
@@ -1,18 +1,48 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShakeController : MonoBehaviour
+public class CameraShakeController : MonoSingleton<CameraShakeController>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private CinemachineVirtualCamera _playerCam;
+    private CinemachineBasicMultiChannelPerlin _perlin;
+
+    private float _power, _duration;
+    private Coroutine _shakeRoutine;
+
+    private void Awake()
     {
-        
+        _perlin = _playerCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            ShakeCam(10, 0.1f);
+        }
+    }
+
+    public void ShakeCam(float power, float duration)
+    {
+        _power = power;
+        _duration = duration;
+
+        if (_shakeRoutine != null)
+            StopCoroutine(_shakeRoutine);
+
+        _shakeRoutine = StartCoroutine(ShakeRoutine());
+    }
+
+    private IEnumerator ShakeRoutine()
+    {
+        _perlin.m_AmplitudeGain = _power;
+        _perlin.m_FrequencyGain = _power;
+
+        yield return new WaitForSeconds(_duration);
+
+        _perlin.m_AmplitudeGain = 0;
+        _perlin.m_FrequencyGain = 0;
     }
 }
