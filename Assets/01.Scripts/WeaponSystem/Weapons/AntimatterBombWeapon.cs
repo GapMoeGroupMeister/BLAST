7c1f682e6006d9496eb5dc14d0b34c47c88384a1
Leooo <123617759+Leooo918@users.noreply.github.com>
2024-09-13 00:06:20 +0900

    feat: antimatterbomb

--- Assets/01.Scripts/WeaponSystem/Weapons/AntimatterBombWeapon.cs
+++ Assets/01.Scripts/WeaponSystem/Weapons/AntimatterBombWeapon.cs
@@ -2,12 +2,37 @@
 
 public class AntimatterBombWeapon : Weapon
 {
+    [SerializeField] private AntimatterBomb bombPf;
+    [SerializeField] private float _detectingRange = 40f;
+    private Collider[] _enemyColl;
+
+    private void Awake()
+    {
+        _enemyColl = new Collider[32];
+    }
+
     public override bool UseWeapon()
     {
-        if(base.UseWeapon())
+        if (base.UseWeapon())
         {
-            //여기에 로직
-        }	
+            int enemCnt = Physics.OverlapSphereNonAlloc(player.transform.position, _detectingRange, _enemyColl, whatIsEnemy);
+
+            if (enemCnt > 0)
+            {
+                Collider closestColl = _enemyColl[0];
+                for (int i = 1; i < enemCnt; i++)
+                {
+                    float prevDist = Vector3.Distance(player.transform.position, closestColl.transform.position);
+                    float newDist = Vector3.Distance(player.transform.position, _enemyColl[i].transform.position);
+
+                    if (newDist < prevDist) closestColl = _enemyColl[i];
+                }
+
+                return true;
+            }
+
+            return false;
+        }
 
         return true;
     }
@@ -15,5 +40,12 @@ public class AntimatterBombWeapon : Weapon
     protected override void Update()
     {
         base.Update();
+    }
+
+    protected void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        if (player != null)
+            Gizmos.DrawWireSphere(player.transform.position, _detectingRange);
     }
 }
