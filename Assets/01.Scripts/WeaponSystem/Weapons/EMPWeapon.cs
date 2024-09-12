311df624fd279f9783a5e35c1ef3580680b96b72
leo82380 <tkdrb062@gmail.com>
2024-09-12 19:58:13 +0900

    feat: EMP Particle

--- Assets/01.Scripts/WeaponSystem/Weapons/EMPWeapon.cs
+++ Assets/01.Scripts/WeaponSystem/Weapons/EMPWeapon.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 public class EMPWeapon : Weapon
 {
     [SerializeField] private SphereDamageCaster _damageCaster;
+    [SerializeField] private ParticleSystem _explosionEffect;
+
+    public override void WeaponInit()
+    {
+        if (!player)
+            player = FindObjectOfType<Player>();
+    }
+
     public override bool UseWeapon()
     {
         if(base.UseWeapon())
         {
+            WeaponInit();
             _damageCaster.CastDamage((int)(level * 10));
+            if (player == null) Debug.LogError("Player is null");
+            ParticleSystem explosionEffect = Instantiate(_explosionEffect, player.transform.position, Quaternion.identity);
+            explosionEffect.Play();
             // Stun effect
         }	
 
