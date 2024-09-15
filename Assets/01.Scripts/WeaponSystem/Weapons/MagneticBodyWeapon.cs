2b50814eb19c3b75646cbbef14e0e1e660a06625
Crogen0407 <mymcoding0407@gmail.com>
2024-09-15 15:28:04 +0900

    feat : MagneticBodyWeapon

--- Assets/01.Scripts/WeaponSystem/Weapons/MagneticBodyWeapon.cs
+++ Assets/01.Scripts/WeaponSystem/Weapons/MagneticBodyWeapon.cs
@@ -2,14 +2,23 @@
 
 public class MagneticBodyWeapon : Weapon
 {
+    [SerializeField] private Transform _playerXPBodyTrm;
+
     public override bool UseWeapon()
     {
         if(base.UseWeapon())
         {
-            //여기에 로직
+            SetScaleForPlayerXPBody();
         }	
 
         return true;
+    }
+
+    private void SetScaleForPlayerXPBody()
+	{
+        float scale = 1f + 2 * (level / 10f);
+
+        _playerXPBodyTrm.localScale = new Vector3(scale, 1, scale);
     }
 
     protected override void Update()
