8d018fc998701510b2183eff80c236d86a3b93e6
leo82380 <tkdrb062@gmail.com>
2024-09-15 16:02:10 +0900

    feat: WaveSystem End

---
+++ Assets/01.Scripts/Combat/WaveSystem/WaveSO.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/WaveSO")]
+public class WaveSO : ScriptableObject
+{
+    public List<WaveEnemy> waveEnemies;
+}
