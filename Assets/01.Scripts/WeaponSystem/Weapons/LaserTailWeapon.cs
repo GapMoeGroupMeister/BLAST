95d06fd01044f00bdd5b0c977cc1b851754c33a6
Crogen0407 <mymcoding0407@gmail.com>
2024-09-16 16:55:11 +0900

    feat : LaserTailPoint

--- Assets/01.Scripts/WeaponSystem/Weapons/LaserTailWeapon.cs
+++ Assets/01.Scripts/WeaponSystem/Weapons/LaserTailWeapon.cs
@@ -1,12 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class LaserTailWeapon : Weapon
 {
-    public override bool UseWeapon()
+    [SerializeField] private LaserTailPoint _laserTailPointPrefab;
+    private List<Transform> _pointTargets;
+    private int _currentDroneCount = 0;
+
+	private void Start()
+	{
+        _pointTargets = new List<Transform>();
+        _pointTargets.Add(GameManager.Instance.Player.transform);
+    }
+
+	public override bool UseWeapon()
     {
         if(base.UseWeapon())
         {
-            //여기에 로직
+            if (level > _currentDroneCount)
+            {
+                var tailPoint = Instantiate(_laserTailPointPrefab);
+                tailPoint.target = _pointTargets[(int)level - 1];
+                _pointTargets.Add(tailPoint.transform);
+                tailPoint.Init(level, this);
+                ++_currentDroneCount;
+            }
         }	
 
         return true;
