78d857826f9b6eaef6cd64c788648578cc3f0fd4
leo82380 <tkdrb062@gmail.com>
2024-09-20 00:33:25 +0900

    feat: DestructionBeam

---
+++ Assets/01.Scripts/WeaponSystem/DestructionBeam.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DestructionBeam : WeaponEffect
+{
+    [SerializeField] private ParticleSystem _chargeEffect;
+    [SerializeField] private ParticleSystem _beamEffect;
+    [SerializeField] private BoxDamageCaster _damageCaster;
+    
+    [ContextMenu("StartBeam")]
+    public void TestB()
+    {
+        StartBeam(1, 1, transform);
+    }
+    
+    public void StartBeam(float chargeTime, float beamDuration, Transform t)
+    {
+        transform.SetParent(t);
+        StartCoroutine(ChargeBeam(chargeTime, beamDuration));
+    }
+    
+    private IEnumerator ChargeBeam(float chargeTime, float beamDuration)
+    {
+        _chargeEffect.Play();
+        yield return new WaitForSeconds(chargeTime);
+        _beamEffect.Play();
+        yield return new WaitForSeconds(beamDuration);
+        _beamEffect.Stop();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_beamEffect.isPlaying)
+        {
+            _damageCaster.CastDamage(_damage);
+        }
+    }
+}
--- Assets/01.Scripts/WeaponSystem/Weapons/DestructionBeamWeapon.cs
+++ Assets/01.Scripts/WeaponSystem/Weapons/DestructionBeamWeapon.cs
@@ -1,59 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DestructionBeamWeapon : Weapon
 {
-    [SerializeField] private ParticleSystem _chargeEffect;
-    [SerializeField] private ParticleSystem _beamEffect;
-    [SerializeField] private Transform[] _boxDamageCaster;
-    [SerializeField] private Transform _parent;
-    [SerializeField] private int _damage = 1;
+    [SerializeField] private DestructionBeam _destructionBeam;
+    [SerializeField] private List<Transform> _beamTrm;
+    
     [SerializeField] private float _chargeTime = 1;
     [SerializeField] private float _beamDuration = 1;
+
+    private void Awake()
+    {
+        _beamTrm.AddRange(GameManager.Instance.Player.currentPlayerPart.magazineInfoL.bulletFirePositions);
+        _beamTrm.AddRange(GameManager.Instance.Player.currentPlayerPart.magazineInfoR.bulletFirePositions);
+    }
 
     public override bool UseWeapon()
     {
         if(base.UseWeapon())
         {
-            StartCoroutine(ChargeBeam());
+            foreach (var trm in _beamTrm)
+            {
+                DestructionBeam destructionBeam = Instantiate(_destructionBeam, trm.position, trm.rotation);
+                destructionBeam.StartBeam(_chargeTime, _beamDuration, trm);
+                Destroy(destructionBeam.gameObject, _chargeTime + _beamDuration);
+            }
         }	
 
         return true;
-    }
-
-    private IEnumerator ChargeBeam()
-    {
-        foreach (var t in _boxDamageCaster)
-        {
-            var chargeEffect = Instantiate(_chargeEffect, t.transform);
-            //chargeEffect.transform.rotation = player. 
-            chargeEffect.Play();
-            Destroy(chargeEffect.gameObject, _chargeTime + _beamDuration);
-        }
-        yield return new WaitForSeconds(_chargeTime);
-        yield return StartCoroutine(FireBeam());
-    }
-
-    private IEnumerator FireBeam()
-    {
-        foreach (var t in _boxDamageCaster)
-        {
-            var beamEffect = Instantiate(_beamEffect, t.transform);
-            beamEffect.Play();
-            Destroy(beamEffect.gameObject, _beamDuration + 1);
-        }
-        float timer = 0;
-        while (timer < _beamDuration)
-        {
-            // foreach (var boxDamageCaster in _boxDamageCaster)
-            // {
-            //     boxDamageCaster.CastDamage(_damage);
-            //     yield return null;
-            // }
-            Debug.Log("Destruction Beam Damage");
-            timer += Time.deltaTime;
-            yield return null;
-        }
     }
 }
