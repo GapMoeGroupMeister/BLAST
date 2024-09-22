859065f68811b8ae0221c46d490d4ca6ed157f37
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-09-23 01:40:20 +0900

    feat : Enemy Burned 연출... 이였던것.

--- Assets/01.Scripts/Agent/Enemy/DragoonEnemy/States/DragoonEnemyDeadState.cs
+++ Assets/01.Scripts/Agent/Enemy/DragoonEnemy/States/DragoonEnemyDeadState.cs
@@ -1,17 +1,43 @@
+using Crogen.ObjectPooling;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DragoonEnemyDeadState : EnemyState<DragoonEnemy>
 {
+    private Material _dissolveMat;
+    private readonly int _burnedHash = Shader.PropertyToID("_Burned");
+
     public DragoonEnemyDeadState(DragoonEnemy enemyBase, EnemyStateMachine<DragoonEnemy> stateMachine, string animBoolName) : base(enemyBase, stateMachine, animBoolName)
     {
+        _dissolveMat = enemyBase.MeshRendererCompo.material;
     }
-
 
     public override void Enter()
     {
         base.Enter();
         _enemyBase.EnemyMovementCompo.DisableNavAgent();
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+        if(IsTriggerCalled(AnimationTriggerEnum.EndTrigger))
+        {
+            _enemyBase.StartCoroutine(BurnedCoroutine());
+            RemoveTrigger(AnimationTriggerEnum.EndTrigger);
+        }
+    }
+
+    private IEnumerator BurnedCoroutine()
+    {
+        float percent = 0;
+        while(percent < 1)
+        {
+            _dissolveMat.SetFloat(_burnedHash, percent);
+            percent += Time.deltaTime * 2;
+            yield return null;
+        }
+        _enemyBase.Push();
     }
 }
