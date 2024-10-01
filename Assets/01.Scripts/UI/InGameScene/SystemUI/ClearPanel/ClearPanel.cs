ed40c9ae94959789340f972502bfc6d0c65bfd5d
leo82380 <tkdrb062@gmail.com>
2024-10-02 00:49:57 +0900

    feat: ClearPanel

--- Assets/01.Scripts/UI/InGameScene/SystemUI/ClearPanel/ClearPanel.cs
+++ Assets/01.Scripts/UI/InGameScene/SystemUI/ClearPanel/ClearPanel.cs
@@ -1,4 +1,12 @@
-﻿public class ClearPanel
+﻿using TMPro;
+using UnityEngine;
+
+public class ClearPanel : UIPanel
 {
+    [SerializeField] private TextMeshProUGUI _levelText;
+    [SerializeField] private TextMeshProUGUI _coinText;
+    [SerializeField] private TextMeshProUGUI _servivedText;
+    [SerializeField] private TextMeshProUGUI _rankText;
     
+    [SerializeField] private Transform _upgradesParent;
 }
