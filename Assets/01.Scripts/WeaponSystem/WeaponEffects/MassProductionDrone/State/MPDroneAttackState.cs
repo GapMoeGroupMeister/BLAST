ceb1229a628099cf4f376ea9150f7bdb4812eba9
Crogen0407 <mymcoding0407@gmail.com>
2024-10-08 01:04:04 +0900

    fix : NavMesh Surface

--- Assets/01.Scripts/WeaponSystem/WeaponEffects/MassProductionDrone/State/MPDroneAttackState.cs
+++ Assets/01.Scripts/WeaponSystem/WeaponEffects/MassProductionDrone/State/MPDroneAttackState.cs
@@ -7,12 +7,16 @@ public class MPDroneAttackState : MPDroneState
 	private float _attackDelay = 1f;
 	private float _curAttackTime = 0f;
 
+	private Vector3 _enterPos;
+	private Vector3 _targetLocalPos;
 	public MPDroneAttackState(MassProductionDrone mpDrone, MPDroneStateMachine mpDroneStateMachine) : base(mpDrone, mpDroneStateMachine)
 	{
 	}
 
 	public override void Enter()
 	{
+		_enterPos = _mpDrone.transform.position;
+		_targetLocalPos = _enterPos - _mpDrone.currentTarget.position;
 	}
 
 	public override void Exit()
@@ -27,11 +31,6 @@ public class MPDroneAttackState : MPDroneState
 			_mpDrone.attackCompo.OnAttack(_mpDrone.currentTarget.position);
 		}
 
-		float distance = Vector3.Distance(_mpDrone.currentTarget.position, _mpDrone.transform.position);
-
-		if(distance > 4.5f)
-		{
-			_stateMachine.ChangeState(MPDroneStateType.Move);
-		}
+		_mpDrone.transform.position = _targetLocalPos + _mpDrone.currentTarget.position;
 	}
 }
