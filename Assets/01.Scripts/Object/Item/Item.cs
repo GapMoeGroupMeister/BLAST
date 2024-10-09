d9cd86e16f45cf09d95ccd82501d5292b2b8af78
Crogen0407 <mymcoding0407@gmail.com>
2024-10-10 02:32:50 +0900

    fix : Item Drop error

--- Assets/01.Scripts/Object/Item/Item.cs
+++ Assets/01.Scripts/Object/Item/Item.cs
@@ -50,7 +50,7 @@ namespace ItemManage
         
         public Vector3 GetRandomPosition(float radius)
         {
-            var randomPos = UnityEngine.Random.insideUnitSphere * radius;
+            var randomPos = UnityEngine.Random.insideUnitSphere * radius + transform.position;
             randomPos.y = 0;
             return randomPos;
         }
