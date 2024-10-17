7d0aaeee737216606036bef0b728ea4b42898c48
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-10-18 02:01:41 +0900

    feat: LobbyButton Description

--- Assets/01.Scripts/UI/LobbyScene/MenuUI/ButtonDescriptionPanel.cs
+++ Assets/01.Scripts/UI/LobbyScene/MenuUI/ButtonDescriptionPanel.cs
@@ -1,12 +1,32 @@
-﻿using TMPro;
+﻿using System;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class ButtonDescriptionPanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _descriptionText;
-    
+    [SerializeField] private float _defaultXPos, _activeXPos, _duration;
+    private RectTransform _rectTrm;
+    private Tween _tween;
+
+    private void Awake()
+    {
+        _rectTrm = transform as RectTransform;
+        
+    }
+
     public void Show(string content)
     {
+        //if(_tween != null) _tween.Complete();
         _descriptionText.text = content;
+        _tween = _rectTrm.DOAnchorPosX(_activeXPos, _duration);
+    }
+
+    public void Disable()
+    {
+        //if(_tween != null) _tween.Complete();
+        _tween = _rectTrm.DOAnchorPosX(_defaultXPos, _duration).SetEase(Ease.InQuint);
+        
     }
 }
--- Assets/01.Scripts/UI/LobbyScene/MenuUI/MenuButton.cs
+++ Assets/01.Scripts/UI/LobbyScene/MenuUI/MenuButton.cs
@@ -1,19 +1,21 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MenuButton : MonoBehaviour, IPointerEnterHandler
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Button _button;
     [SerializeField] private string _content;
-    //public UnityEvent<string> OnSeelctEvent;
     [SerializeField] private ButtonDescriptionPanel _descriptionPanel;
 
     
     public void OnPointerEnter(PointerEventData eventData)
     {
         _descriptionPanel.Show(_content);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _descriptionPanel.Disable();
     }
 }
