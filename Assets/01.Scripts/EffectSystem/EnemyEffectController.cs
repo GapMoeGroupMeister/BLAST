395ccfb008d695e8cdca9b66603b77c011c251f2
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-11-01 20:24:17 +0900

    feat: Player EffectState Slot Pooling

--- Assets/01.Scripts/EffectSystem/EnemyEffectController.cs
+++ Assets/01.Scripts/EffectSystem/EnemyEffectController.cs
@@ -1,20 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using EffectSystem;
 using UnityEngine;
 
-public class EnemyEffectController : AgentEffectController
+namespace EffectSystem
 {
-    [SerializeField] private EnemyEffectStateUI _effectUI;
-    // 불나방, 
-
-    public override void ApplyEffect(EffectStateTypeEnum type, float duration, int level, float percent = 1f)
+    public class EnemyEffectController : AgentEffectController
     {
-        if(type == 0) return;
-        if(!effectDictionary[type].enabled)
-            _effectUI.GenerateSlot(type, effectDictionary[type]);
+        [SerializeField] private EnemyEffectStateUI _effectUI;
+
+        public override void ApplyEffect(EffectStateTypeEnum type, float duration, int level, float percent = 1f)
+        {
+            if(type == 0) return;
+            if(!effectDictionary[type].enabled)
+                _effectUI.GenerateSlot(type, effectDictionary[type]);
             
-        base.ApplyEffect(type, duration, level);
+            base.ApplyEffect(type, duration, level);
         
+        }
     }
+
 }
