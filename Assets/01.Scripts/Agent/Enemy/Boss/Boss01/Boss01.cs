6c4d5893e23ccd45c8f48b040f9ba890cd8da016
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-12-29 21:32:44 +0900

    feat : Boss01 Animations, Boss01 LaserSkill, fix : Boss01 ShootBulletSkill Bug,

--- Assets/01.Scripts/Agent/Enemy/Boss/Boss01/Boss01.cs
+++ Assets/01.Scripts/Agent/Enemy/Boss/Boss01/Boss01.cs
@@ -9,19 +9,37 @@ public class Boss01 : Boss
     [SerializeField]
     private Color _laserPointerColor, _endColor;
     private readonly int _emissionColorID = Shader.PropertyToID("_EmissionColor");
+    [SerializeField]
+    private LaserAligner _laserAligner;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _laserAligner.transform.SetParent(null);
+    }
 
     public void ShootBulletEffect(float duration)
     {
         for(int i = 0; i < _laserPointers.Length; i++)
         {
-            _laserPointers[i].gameObject.SetActive(true);
-            Material material = _laserPointers[i].material;
+            var pointer = _laserPointers[i];
+            pointer.gameObject.SetActive(true);
+            Material material = pointer.material;
             material.SetColor(_emissionColorID, _laserPointerColor);
-            material.DOColor(_endColor, _emissionColorID, duration).OnComplete(() =>
+            material.DOColor(_endColor, _emissionColorID, duration).OnComplete(() => 
             {
-                _laserPointers[i].gameObject.SetActive(false);
+                material.SetColor(_emissionColorID, _laserPointerColor);
+                material.DOColor(_endColor, _emissionColorID, 0.15f);
             });
-
+            DOVirtual.DelayedCall(duration + 0.15f, () => pointer.gameObject.SetActive(false));
         }
+    }
+
+    public void EnableLaser()
+    {
+        //todo : 레이저 갯수 Level 기준으로 다시 변경하기
+        //int count = Mathf.CeilToInt(Level * 3) + 1;
+        int count = 3;
+        _laserAligner.SetActiveLaser(true, count);
     }
 }
